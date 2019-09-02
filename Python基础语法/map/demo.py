93f6048988b13878ec0adce8d8b86519b99ac716
ddxygq <2601538122@qq.com>
2019-09-02 14:31:44 +0800

    获取图片的拍摄时间、地点

---
+++ Python基础语法/map/demo.py
@@ -0,0 +1,23 @@
+import exifread
+import re
+
+def  imageread():
+        GPS = {}
+        date = ''
+        f = open("0.jpg",'rb')
+        imagetext = exifread.process_file(f)
+        print(imagetext)
+        for key in imagetext:                           #打印键值对
+                print(key,":",imagetext[key])
+        print('********************************************************\n********************************************************')
+        for q in imagetext:                             #打印该图片的经纬度 以及拍摄的时间
+                if q == "GPS GPSLongitude":
+                        print("GPS经度 =", imagetext[q],imagetext['GPS GPSLatitudeRef'])
+                elif q =="GPS GPSLatitude":
+                        print("GPS纬度 =",imagetext[q],imagetext['GPS GPSLongitudeRef'])
+                elif q =='Image DateTime':
+                        print("拍摄时间 =",imagetext[q])
+
+
+if __name__ == '__main__':
+    imageread()
