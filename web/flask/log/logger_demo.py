833ce1fddee0c67e40bff8af779093ef0d5a50c1
ddxygq <2601538122@qq.com>
2019-09-16 12:43:54 +0800

    Python异常

--- web/flask/log/logger_demo.py
+++ web/flask/log/logger_demo.py
@@ -1,4 +1,8 @@
 from flask import Flask,redirect,url_for
+import logging
+from logging.handlers import RotatingFileHandler
+from logging import Formatter
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 
@@ -9,19 +13,20 @@ ADMINS = ['2864791604@qq.com']
 这种设置，INFO以上级别的日志都会保存到文件，ERROR级别的日志会通过邮件发送。
 """
 if not app.debug:
-    import logging
+    # 这个如果不设置，日志中就没有INFO级别的日志
     app.logger.setLevel(logging.INFO)
-    from logging.handlers import RotatingFileHandler
-    from logging import Formatter
-    handler = RotatingFileHandler('flask.log', maxBytes=1024 * 1024 * 100, backupCount=10)
-    handler.setLevel(logging.INFO)
+
+    # 设置日志格式
     formatter = Formatter('%(asctime)s %(levelname)s: %(module)s.%(funcName)s() %(message)s '
                           '[in %(pathname)s:%(lineno)d]')
+
+    # INFO级别日志
+    handler = RotatingFileHandler('flask.log', maxBytes=1024 * 1024 * 100, backupCount=10)
+    handler.setLevel(logging.INFO)
     handler.setFormatter(formatter)
     app.logger.addHandler(handler)
 
     # 设置邮件日志
-    from logging.handlers import SMTPHandler
     mail_handler = SMTPHandler('smtp.126.com'
                                , 'ikeguang@126.com'
                                , ADMINS
