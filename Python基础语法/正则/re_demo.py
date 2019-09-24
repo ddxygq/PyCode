4e08f67946001b1341f2b476210a7b8d77a401ce
ddxygq <2601538122@qq.com>
2019-09-24 18:06:22 +0800

    正则

--- Python基础语法/正则/re_demo.py
+++ Python基础语法/正则/re_demo.py
@@ -1,8 +1,31 @@
 import re
 
-p = re.compile('[a-z]+')
 
-print(p.pattern)
-result = p.match("abc123abc")
+def match_demo():
+    p = re.compile('[a-z]+')
 
-print(result.group())
+    print(p.pattern)
+    result = p.match("abc123abc")
+    print(result)
+    print(result.span())
+    print(result.string)
+    print(result.group())
+
+
+def search_demo():
+    p = re.compile('[a-z]+')
+    result = p.match("abc123def")
+    print(result)
+    print(result.group())
+
+
+def findall_demo():
+    p = re.compile('[a-z]+')
+    result = p.findall("abc123abc")
+    print(result)
+
+
+if __name__ == '__main__':
+    # match_demo()
+    # search_demo()
+    findall_demo()
