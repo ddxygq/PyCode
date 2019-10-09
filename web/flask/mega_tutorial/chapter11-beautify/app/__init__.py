7566e8a6b7e16ee213a12b13107dcf2ee507445e
ddxygq <2601538122@qq.com>
2019-10-09 20:30:17 +0800

    使用bootstrap美化

---
+++ web/flask/mega_tutorial/chapter11-beautify/app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login.login_manager import LoginManager
+from app.log import logging_module
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config.from_object(Config)
+# app.secret_key = 'afjah3ur38thgh'
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+# 日志
+app.logger = logging_module.getLogger('log/log.log', 'mylog')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# 邮件
+mail = Mail(app)
+
+# css框架Bootstrap
+bootstrap = Bootstrap(app)
+
---
+++ web/flask/mega_tutorial/chapter11-beautify/app/forms.py
@@ -0,0 +1,74 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+
+
+import sys
+sys.path.append('../')
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+
+class EditProfileForm(FlaskForm):
+    """
+    个人资料编辑
+    """
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('提交')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+
+class PostForm(FlaskForm):
+    """
+    发布post
+    """
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('提交')
+
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
